0604ecac01b2b6a37f8775f8e9f7eaa74215c69d
盧俊廷 <lutim102030@gmail.com>
2022-03-30 22:55:10 +0800

    compose paging

---
+++ app/src/main/java/tim/composepaging/MainViewModel.kt
@@ -0,0 +1,57 @@
+package tim.composepaging
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    var state by mutableStateOf(ScreenState())
+    val repository = Repository()
+
+    private val paginator = DefaultPaginator(
+        initialKey = state.page,
+        onLoadUpdated = {
+            state = state.copy(isLoading = it)
+        },
+        onRequest = { nextPage ->
+            repository.getItems(nextPage, 10)
+        },
+        getNextKey = {
+            state.page + 1
+        },
+        onError = {
+            state = state.copy(error = it?.localizedMessage)
+        },
+        onSuccess = { items, newKey ->
+            state = state.copy(
+                isLoading = false,
+                items = state.items + items,
+                page = newKey,
+                endReached = items.isEmpty()
+            )
+        }
+    )
+
+    init {
+        loadNextItems()
+    }
+
+    fun loadNextItems() {
+        viewModelScope.launch {
+            paginator.loadNextItems()
+        }
+    }
+
+}
+
+data class ScreenState(
+    val isLoading: Boolean = false,
+    val items: List<ListItem> = emptyList(),
+    val error: String? = null,
+    val endReached: Boolean = false,
+    val page: Int = 0
+)
